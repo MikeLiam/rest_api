abec0f0f19d414a0db14e041fd5f9ffc59b60dfd
MikeLiam <miguelalcalaromero@gmail.com>
2020-10-15 14:17:48 +0200

    seeders

--- seeders/20201015103253-seed-user.js
+++ seeders/20201015103253-seed-user.js
@@ -4,16 +4,20 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        "firstName": "Joe",
-        "lastName": "Smith",
-        "emailAddress": "joe@smith.com",
-        "password": "joepassword"
+        firstName: "Joe",
+        lastName: "Smith",
+        emailAddress: "joe@smith.com",
+        password: "joepassword",
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
-        "firstName": "Sally",
-        "lastName": "Jones",
-        "emailAddress": "sally@jones.com",
-        "password": "sallypassword"
+        firstName: "Sally",
+        lastName: "Jones",
+        emailAddress: "sally@jones.com",
+        password: "sallypassword",
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
